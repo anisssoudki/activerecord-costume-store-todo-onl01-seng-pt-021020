febef30cc40068ca4e9e5f44028337a23051a51f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-09 18:29:12 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :still_in_business
+      t.datetime :open_time
+      t.datetime :close_time
+    end
+  end
+end
