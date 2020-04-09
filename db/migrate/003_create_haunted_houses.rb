4809dea620e3b4cd4a7e40c6a7eddf1f1ff49287
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-09 18:33:27 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,17 +1,16 @@
 # Create your haunted_houses migration here
 
-class Haunted_house < ActiveRecord::Migration[5.1]
-  
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name 
-      t.text :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly
-      t.datetime :open_time
-      t.datetime :close_time
-      t.text :description
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.text :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.datetime :opening_date
+            t.datetime :closing_date
+            t.text :description
+        end
     end
-  end
 end
